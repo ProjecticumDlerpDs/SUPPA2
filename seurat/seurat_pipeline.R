21d26a15ccd8417bd5226a1730e89a17632b3b0b
Jalisa van der Zeeuw <jalisavanderzeeuw@hotmail.com>
2025-05-30 15:38:25 +0200

    Herstel

--- seurat/seurat_pipeline.R
+++ seurat/seurat_pipeline.R
@@ -7,6 +7,6 @@ library(tidyr)
 library(Seurat)
 
 #inladen data
-features_metadata <- read.csv("/Users/jalisavanderzeeuw/Desktop/Desktop - Jalisa/Rstudio/suppa2/seurat/data/feature_metadata.csv")
-samples_metadata <- read.csv("/Users/jalisavanderzeeuw/Desktop/Desktop - Jalisa/Rstudio/suppa2/seurat/data/e85_sample_metadata.csv")
+features_metadata <- read.csv("/Users/jalisavanderzeeuw/Desktop/DesktopJalisa/Rstudio/suppa2/seurat/data/e85_feature_metadata.csv.gz")
+samples_metadata <- read.csv("/Users/jalisavanderzeeuw/Desktop/DesktopJalisa/Rstudio/suppa2/seurat/data/e85_sample_metadata.csv")
 
